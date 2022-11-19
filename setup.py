fea6ba7f6fbaab7dce3459e43f9729724d7a6ce8
Alexander Ganslandt <alexanga@axis.com>
2022-11-19 10:47:59 +0100

    Make proper package
    Change-Id: I9855b0283a701408c025e5b98148f71c277803c1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pyhelpers',
+    version='1.0.0',
+    url='https://github.com/Laxen/pyhelpers',
+    author='Alexander Ganslandt',
+    description='Collection of simple helper functions in Python',
+    packages=['pyhelpers']
+)
