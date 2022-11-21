aa7caf34aac2c3ca82b891850aed5670c74955a6
Alexander Ganslandt <alexanga@axis.com>
2022-11-21 18:48:55 +0100

    Clean up and document
    Change-Id: Iadcb2856112c5e6470b74086857b31bb7714fa33

--- pyhelpers/__init__.py
+++ pyhelpers/__init__.py
@@ -5,6 +5,16 @@ class Coord:
         self.x = int(x)
         self.y = int(y)
 
+    # Returns a Coord from a string with format "x,y"
+    @classmethod
+    def from_string(cls, string):
+        x, y = string.split(",")
+        return cls(x, y)
+
+    # Returns the squared distance from this Coord to another Coord
+    def distance2_to(self, other):
+        return (self.x - other.x)**2 + (self.y - other.y)**2
+
     def __repr__(self):
         return "({},{})".format(self.x, self.y)
 
@@ -25,14 +35,6 @@ class Coord:
     def __hash__(self):
         return hash((self.x, self.y))
 
-    def distance2_to(self, other):
-        return (self.x - other.x)**2 + (self.y - other.y)**2
-
-    @classmethod
-    def from_string(cls, string):
-        x, y = string.split(",")
-        return cls(x, y)
-
 class Grid2D:
     def __init__(self, width, height, element):
         self.width = width
@@ -43,6 +45,7 @@ class Grid2D:
             for x in range(self.width):
                 self[x, y] = element
 
+    # Returns a Grid2D with elements initialized from a 2D list
     @classmethod
     def from_2d_list(cls, list_2d):
         grid = Grid2D(len(list_2d[0]), len(list_2d), 0)
@@ -50,6 +53,8 @@ class Grid2D:
             grid[c] = list_2d[c.y][c.x]
         return grid
 
+    # Returns a Grid2D with Coords from coords_list set to element and the rest
+    # set to 0
     @classmethod
     def from_coords(cls, coords_list, element):
         grid = Grid2D(0, 0, 0)
@@ -60,59 +65,7 @@ class Grid2D:
 
         return grid
 
-    def __repr__(self):
-        string = ""
-        for y in range(self.height):
-            for x in range(self.width):
-                e = self[x, y]
-                if e is not None:
-                    string += str(e) + " "
-                else:
-                    string += ". "
-            string += "\n"
-
-        return string
-
-    def __iter__(self):
-        for y in range(self.height):
-            for x in range(self.width):
-                yield Coord(x,y), self[x, y]
-
-    def __getitem__(self, keys):
-        if isinstance(keys, tuple):
-            if isinstance(keys[0], slice):
-                xslice = keys[0].indices(self.width)
-                yslice = keys[1].indices(self.height)
-
-                matrix = []
-                for y in range(yslice[0], yslice[1]):
-                    row = []
-                    for x in range(xslice[0], xslice[1]):
-                        row.append(self[x, y])
-                    matrix.append(row)
-
-                new_grid = Grid2D.from_2d_list(matrix)
-
-                return new_grid
-            else:
-                if keys in self.grid:
-                    return self.grid[Coord(keys[0], keys[1])]
-            return None
-        elif isinstance(keys, Coord):
-            if keys in self.grid:
-                return self.grid[keys]
-            return None
-        else:
-            raise TypeError("Grid can't get element with key type {}".format(type(keys)))
-
-    def __setitem__(self, keys, item):
-        if isinstance(keys, tuple):
-            self.grid[Coord(keys[0], keys[1])] = item
-        elif isinstance(keys, Coord):
-            self.grid[keys] = item
-        else:
-            raise TypeError("Grid can't set element with key type {}".format(type(keys)))
-
+    # Returns a dict with the neighbors for a given Coord
     def neighbors(self, coord, diagonal=True):
         n = dict()
         x = coord.x
@@ -137,6 +90,7 @@ class Grid2D:
 
         return n
 
+    # Returns the number of times an element occurs in the grid
     def count(self, element):
         count = 0
         for _, d in self:
@@ -144,6 +98,7 @@ class Grid2D:
                 count += 1
         return count
 
+    # Returns a list of Coords containing the shortest path from start to end
     def find_path(self, start, end):
         class Node:
             def __init__(self, coords, cost):
@@ -201,8 +156,65 @@ class Grid2D:
 
         return path
 
+    # Returns the grid as a list
     def flatten(self):
         flat = []
         for _, e in self:
             flat.append(e)
         return flat
+
+    def __repr__(self):
+        string = ""
+        for y in range(self.height):
+            for x in range(self.width):
+                e = self[x, y]
+                if e is not None:
+                    string += str(e) + " "
+                else:
+                    string += ". "
+            string += "\n"
+
+        return string
+
+    def __iter__(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                yield Coord(x,y), self[x, y]
+
+    def __getitem__(self, keys):
+        if isinstance(keys, tuple):
+            # Tuple can be both (x,y) and a slice
+            if isinstance(keys[0], slice):
+                # Slice
+                xslice = keys[0].indices(self.width)
+                yslice = keys[1].indices(self.height)
+
+                matrix = []
+                for y in range(yslice[0], yslice[1]):
+                    row = []
+                    for x in range(xslice[0], xslice[1]):
+                        row.append(self[x, y])
+                    matrix.append(row)
+
+                new_grid = Grid2D.from_2d_list(matrix)
+                return new_grid
+            else:
+                # Tuple
+                if keys in self.grid:
+                    return self.grid[Coord(keys[0], keys[1])]
+            return None
+        elif isinstance(keys, Coord):
+            # Coord
+            if keys in self.grid:
+                return self.grid[keys]
+            return None
+        else:
+            raise TypeError("Grid can't get element with key type {}".format(type(keys)))
+
+    def __setitem__(self, keys, item):
+        if isinstance(keys, tuple):
+            self.grid[Coord(keys[0], keys[1])] = item
+        elif isinstance(keys, Coord):
+            self.grid[keys] = item
+        else:
+            raise TypeError("Grid can't set element with key type {}".format(type(keys)))
